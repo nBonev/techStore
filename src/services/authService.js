bd949cd567a94a459a61622d2523c35e662c2768
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-02-10 16:09:14 +0200

    Check confirm password and user exist

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,16 @@
 import User from '../models/User.js';
 
-const register = (userData) => {
-    User.create(userData); 
+const register = async (userData) => {
+    if(userData.password !== userData.confirmPassword) {
+        throw new Error('Password mismatch!');
+    }
+
+    const user = await User.findOne({ email: userData.email }).select({ _id: true });
+    if(user) {
+        throw new Error('User already exists!');
+    }
+
+    return User.create(userData); 
 };
 
 const authService = {

d596745aee0fb04a809162e4216db21f95b9b129
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-02-11 11:39:27 +0200

    Show dynamic navigation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import handlebars from 'express-handlebars';
 import cookieParser from 'cookie-parser';
 
 import routes from './routes.js';
+import { auth } from './middlewares/authMiddleware.js';
 
 
 const app = express();
@@ -37,6 +38,7 @@ app.set('views', './src/views');
 app.use(express.static('src/public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(auth);
 app.use(routes);
 
 

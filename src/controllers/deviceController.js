9e3f47ffdee8983c8faf5f45eb47db383f9e82a3
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-02-12 14:26:13 +0200

    Add device controller

--- src/controllers/deviceController.js
+++ src/controllers/deviceController.js
@@ -1,9 +1,29 @@
 import { Router } from "express";
+import { isAuth } from "../middlewares/authMiddleware.js";
+import deviceService from "../services/deviceService.js";
+import { getErrorMessage } from "../utils/errorUtils.js";
 
 const deviceController = Router();
 
 deviceController.get('/create', (req, res) => {
     res.render('devices/create');
+});
+
+deviceController.post('/create', isAuth, async (req, res) => {
+    const deviceData = req.body;
+
+    try {
+        await deviceService.create(deviceData);
+
+        res.redirect('/devices');
+    }catch(err) {
+        res.render('devices/create', {
+            error: getErrorMessage(err), 
+            device: deviceData, 
+        });
+    }
+    
+
 });
 
 export default deviceController;

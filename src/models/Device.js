c26ecc1b6c5de71f90a7eb576078778cacfafb1c
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-02-12 13:33:16 +0200

    Add device model

---
+++ src/models/Device.js
@@ -0,0 +1,64 @@
+import { Schema, model, Types } from "mongoose";
+
+const deviceSchema = new Schema({
+    brand: {
+        type: String,
+        required: [true],
+    },
+    model: {
+        type: String,
+        required: [true],
+    },
+    hardDisk: {
+        type: String,
+        required: [true],
+    },
+    screenSize: {
+        type: String,
+        required: [true],
+    },
+    ram: {
+        type: String,
+        required: [true],
+    },
+    operatingSystem: {
+        type: String,
+        required: [true],
+    },
+    cpu: {
+        type: String,
+        required: [true],
+    },
+    gpu: {
+        type: String,
+        required: [true],
+    },
+    price: {
+        type: Number,
+        required: [true],
+    },
+    color: {
+        type: String,
+        required: [true],
+    },
+    weight: {
+        type: String,
+        required: [true],
+    },
+    image: {
+        type: String,
+        required: [true],
+    },
+    preferredList: [{
+        type: Types.ObjectId,
+        ref: 'User',
+    }],
+    owner: {
+        type: Types.ObjectId,
+        ref: 'User',
+    }
+});
+
+const Device = model('Device', deviceSchema);
+
+export default Device;

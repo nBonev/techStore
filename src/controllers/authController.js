4be66d65bc28508a17c4e1350c1254058c0ea5e9
Nikolay <77922872+nBonev@users.noreply.github.com>
2025-02-12 15:24:56 +0200

    Add isGuest middleware

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
 import authService from "../services/authService.js";
 import { AUTH_COOKIE_NAME } from "../config.js";
-import { isAuth } from "../middlewares/authMiddleware.js";
+import { isAuth, isGuest } from "../middlewares/authMiddleware.js";
 import { getErrorMessage } from "../utils/errorUtils.js";
 
 const authController = Router();
 
-authController.get('/login', (req, res) => {
+authController.get('/login', isGuest, (req, res) => {
     res.render('auth/login', {pageTitle: 'TechStore | Login'});
 });
 
-authController.post('/login', async (req, res) => {
+authController.post('/login', isGuest, async (req, res) => {
     const { email, password } = req.body;
 
     try {
@@ -25,11 +25,11 @@ authController.post('/login', async (req, res) => {
     
 });
 
-authController.get('/register', (req, res) => {
+authController.get('/register', isGuest, (req, res) => {
     res.render('auth/register', {pageTitle: 'TechStore | Register'});
 });
 
-authController.post('/register', async (req, res) => {
+authController.post('/register', isGuest, async (req, res) => {
     const userData = req.body;
     
     try {
--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -30,3 +30,12 @@ export const isAuth = (req, res, next) => {
 
     next();
 };
+
+export const isGuest = (req, res, next) => {
+    if(req.user) {
+        return res.redirect('/');
+    }
+
+
+    next();
+};
